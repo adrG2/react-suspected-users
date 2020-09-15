4ecdf4e13bb8221cdcdd873516b16323c81dd304
adrG2 <agarciagarr@uoc.edu>
2020-09-15 14:19:06 +0200

    1 file modified, 1 added

--- src/components/UserButtons.jsx
+++ src/components/UserButtons.jsx
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
+import Example from './Modal';
 
 export default function UserButtons() {
   return (
